73d2c8b508d28ca791d5e96451a6127f1af1b063
arfin-arif <arifmtbd@gmail.com>
2022-09-30 20:54:27 +0600

    product-page-done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,10 +2,32 @@ import React from 'react';
 import './Cart.css'
 const Cart = (props) => {
     const { cart } = props
+
+    let totalPrice = 0;
+    let totalShipping = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        totalPrice = totalPrice + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping
+        quantity = quantity + product.quantity;
+
+    }
+    // useful for only one value
+    // const totalPrice = cart.reduce((accumulator, currentValue) => {
+    //     return (accumulator + currentValue.price);
+    // }, 0);
+    const tax = (totalPrice * 0.1).toFixed(0);
+    const grandTotal = totalPrice + totalShipping + parseInt(tax)
+
     return (
-        <div>
-            <h1>cart</h1>
-            <p>Selected item {cart.length} </p>
+        <div className='cart'>
+            <h4>Order Summary</h4>
+            <p>Selected item {quantity} </p>
+            <p>Total Price: ${totalPrice} </p>
+            <p>Total Shipping : ${totalShipping} </p>
+            <p>Tax : ${tax} </p>
+            <p>Grand Total: <strong>${grandTotal}</strong> </p>
+
         </div>
     );
 };
